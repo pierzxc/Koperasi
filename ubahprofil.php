844638114e91cf36e46ca984509b9136d3e2efb7
PIER-PC\PIER <piersrv@gmail.com>
2018-01-26 12:36:36 +0700

    done

--- ubahprofil.php
+++ ubahprofil.php
@@ -9,61 +9,75 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 session_start();
 include('dbconnect.php');
 $_SESSION['pesan']='Ubah Profil';
+$no_ba = '';
+$fullname = '';
+$email = '';
+$tempat_lahir = '';
+$alamat = '';
+$kota = '';
+$no_ktp = '';
+$telepon = '';
+$pekerjaan = '';
+
+if( isset($_SESSION['no_ba'])!="" ){
+	  $no_ba = $_SESSION['no_ba'];
+}
+$strSQL = mysqli_query($connection,"select * from users where no_ba='".$no_ba."' ");
+if (!$strSQL) {
+	printf("Error: %s\n", mysqli_error($connection));
+	exit();
+}
+if(mysqli_num_rows($strSQL) > 0)
+{
+	while($row = mysqli_fetch_assoc($strSQL)) {
+		$email = $row['email'];
+		$fullname = $row['fullname'];
+		$no_ba =  $row['no_ba'];
+		$tempat_lahir =  $row['tempat_lahir'];
+		$alamat =  $row['alamat'];
+		$kota =  $row['kota'];
+		$no_ktp =  $row['no_ktp'];
+		$telepon =  $row['telepon'];
+		$pekerjaan =  $row['pekerjaan'];
+	}
+}
 if(isset($_POST['action']))
 {          
-    if($_POST['action']=="login")
+
+    if($_POST['action']=="simpan")
     {
-        $username = mysqli_real_escape_string($connection,$_POST['username']);
-        $password = mysqli_real_escape_string($connection,$_POST['password']);
-        $strSQL = mysqli_query($connection,"select username, fullname from users where username='".$username."' and password='".md5($password)."'");
+		$email = mysqli_real_escape_string($connection,$_POST['email']);
+		$fullname = mysqli_real_escape_string($connection,$_POST['fullname']);
+		$tempat_lahir =  mysqli_real_escape_string($connection,$_POST['tempat_lahir']);
+		$alamat =  mysqli_real_escape_string($connection,$_POST['alamat']);
+		$kota =  mysqli_real_escape_string($connection,$_POST['kota']);
+		$no_ktp =  mysqli_real_escape_string($connection,$_POST['no_ktp']);
+		$telepon =  mysqli_real_escape_string($connection,$_POST['telepon']);
+		$pekerjaan =  mysqli_real_escape_string($connection,$_POST['pekerjaan']);
+		
+        $query = 	"UPDATE users 
+					SET email='".$email."',
+					fullname='".$fullname."',
+					tempat_lahir='".$tempat_lahir."',
+					alamat='".$alamat."',
+					kota='".$kota."',
+					no_ktp='".$no_ktp."',
+					telepon='".$telepon."',
+					pekerjaan='".$pekerjaan."'
+					WHERE no_ba='".$no_ba."'";
+        $strSQL = mysqli_query($connection, $query);
         if (!$strSQL) {
 			printf("Error: %s\n", mysqli_error($connection));
 			exit();
 		}
-		//$Results = mysqli_fetch_array($strSQL);
-		$fullname = '';
-        if(mysqli_num_rows($strSQL) > 0)
-        {
-			while($row = mysqli_fetch_assoc($strSQL)) {
-				$fullname = $row['fullname'];
-			}
-            $message = $Results['username']." Login Sucessfully!!";
-			$_SESSION['fullname'] = $fullname;
-			$_SESSION['username'] = $username;
-			header("Location:index.php");
+		else
+		{
+			header("Location:profil.php");
 			exit();
-        }
-        else
-        {
-			$_SESSION['pesan']='USERNAME ATAU PASSWORD SALAH';
-            $message = "Invalid username or password!!";
-        }        
+		}
+		       
     }
-    elseif($_POST['action']=="signup")
-    {
-        $name       = mysqli_real_escape_string($connection,$_POST['username']);
-        $email      = mysqli_real_escape_string($connection,$_POST['email']);
-        $password   = mysqli_real_escape_string($connection,$_POST['password']);
-		$fullname   = mysqli_real_escape_string($connection,$_POST['fullname']);
-        $query = "SELECT email FROM users where email='".$email."'";
-        $result = mysqli_query($connection,$query);
-        $numResults = mysqli_num_rows($result);
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Validate email address
-        {
-            $message =  "Invalid email address please type a valid email!!";
-        }
-        elseif($numResults>=1)
-        {
-            $message = $email." Email already exist!!";
-			$_SESSION['pesan']='REGISTRASI GAGAL, EMAIL SUDAH TERDAFTAR';
-        }
-        else
-        {
-            mysqli_query($connection,"insert into users(username,email,password,fullname) values('".$name."','".$email."','".md5($password)."','".$fullname."')");
-            $message = "Signup Sucessfully!!";
-			$_SESSION['pesan']='REGISTRASI ANDA BERHASIL, SILAHKAN LOGIN';
-        }
-    }
+    
 }
  
 ?>
@@ -72,7 +86,7 @@ if(isset($_POST['action']))
 <!DOCTYPE html>
 <html>
 <head>
-<title>Login</title>
+<title>Ubah Profil</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Credit Login / Register Form Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements" />
@@ -105,18 +119,19 @@ if(isset($_POST['action']))
 			<form action="#" method="post">
 				
 				<div class="form-sub-w3">
-					<input type="text" name="fullname" placeholder="Nama Lengkap " required />
-					<input type="text" name="jenis_kelamin" placeholder="Jenis Kelamin " required />
-					<input type="text" name="jenis_kelamin" placeholder="Jenis Kelamin " required />
-				</div>
-				<div class="form-sub-w3">
-					<input type="text" name="password" class="mail" placeholder="Email" required />
-			
+					<input type="text" name="fullname" placeholder="Nama Lengkap " value="<?php echo $fullname ?>" required />
+					<input type="text" name="tempat_lahir" placeholder="Tempat Lahir " value="<?php echo $tempat_lahir ?>" required />
+					<input type="text" name="alamat" placeholder="Alamat " value="<?php echo $alamat ?>" required />
+					<input type="text" name="email" placeholder="Email " value="<?php echo $email ?>" required />
+					<input type="text" name="kota" placeholder="Kota " value="<?php echo $kota ?>" required />
+					<input type="text" name="no_ktp" placeholder="No.KTP " value="<?php echo $no_ktp ?>" required />
+					<input type="text" name="telepon" placeholder="Telepon " value="<?php echo $telepon ?>" required />
+					<input type="text" name="pekerjaan" placeholder="Pekerjaan " value="<?php echo $pekerjaan ?>" required />
 				</div>
 				
 				
 				<div class="submit-w3l">
-					<input name="action" type="hidden" value="login" />
+					<input name="action" type="hidden" value="simpan" />
 					<input type="submit" value="Simpan">
 				</div>
 				<p class="p-bottom-w3ls"><a  href="./profil.php">Batal</a></p>
